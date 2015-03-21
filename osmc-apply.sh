71e429418bc38e35233616d711b575fab88f2ac0
Sebastian Mueller <balomueller@googlemail.com>
2015-03-21 11:03:37 +0100

    Changed deployment mechanism

--- osmc-apply.sh
+++ osmc-apply.sh
@@ -1,1 +1,10 @@
-scp -r . osmc:puppet & ssh osmc 'sudo puppet apply --test --modulepath=puppet puppet/osmc-manifest.pp'
+#!/bin/bash
+
+set -x
+
+touch package.tar
+ls | grep -v 'package.tar' | xargs tar -u -f package.tar
+scp package.tar osmc:package.tar
+ssh osmc 'mkdir puppet && cd puppet && tar -xf ../package.tar && sudo puppet apply --test --modulepath=. osmc-manifest.pp'
+ssh osmc 'rm -Rf puppet package.tar'
+rm package.tar
