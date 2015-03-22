9518094169e6623c2e54282d6ccda734beaf0798
Sebastian Mueller <balomueller@googlemail.com>
2015-03-22 13:28:36 +0100

    Actually do soemthing, not just test mode

--- osmc-apply.sh
+++ osmc-apply.sh
@@ -3,8 +3,8 @@
 set -x
 
 touch package.tar
-ls | grep -v 'package.tar' | xargs tar -u -f package.tar
+ls | grep -v 'package.tar' | xargs tar -c -f package.tar
 scp package.tar osmc:package.tar
-ssh osmc 'mkdir puppet && cd puppet && tar -xf ../package.tar && sudo puppet apply --test --modulepath=. osmc-manifest.pp'
+ssh osmc 'mkdir puppet && cd puppet && tar -xf ../package.tar && puppet module install --modulepath=../puppet-modules puppetlabs/apt && puppet module install --modulepath=../puppet-modules --ignore-dependencies fortin-tor && sudo puppet apply --debug --modulepath=.:../puppet-modules osmc-manifest.pp'
 ssh osmc 'rm -Rf puppet package.tar'
 rm package.tar
